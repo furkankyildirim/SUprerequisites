6fef0e5665202e1d6accb16713333573c9eb9c29
Ekin Oral <oralekin@sabanciuniv.edu>
2021-12-27 14:53:05 +0300

    add functions for command line use

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,2 +1,24 @@
+from json import load, dump
 from .analysis import Analyze
 from .script import CoursePrerequisites
+
+def doTerm(term):
+    with open("Catalogs/Raws/"+term+".html") as f:
+        CoursePrerequisites(f, term)
+        try:
+            Analyze(term)
+        except Analyze.CycleError as err:
+            e = {}
+            with open('Catalogs/Errors/' + term + '.json', 'r') as fp:
+                e = load(fp)
+            
+            e["cycle"] = str(err.problems)
+            with open('Catalogs/Errors/' + term + '.json', 'w') as fp:
+                dump(e, fp)
+
+
+
+def doTerms(terms):
+    print("doing", terms)
+    for term in terms:
+        doTerm(term)
